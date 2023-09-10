1d40ee4e0a0b402bf8a324fb42513ea4b14895aa
KK <wkx611@outlook.com>
2023-09-10 23:19:16 +0800

    [update] robot structure
    bk robot
    
    [add] test kinematicchain
    
    bk
    
    Update lib
    
    bk

---
+++ 1.2parallel_robot.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lib.robot.robot import KinematicChain, Joint2D, Robot2D, JointType
+
+
+def test_robot_package():
+    fig, ax = plt.subplots()
+    joint1 = Joint2D(JointType.revolute, x=1, y=1, theta=90, l=1, name="joint1")
+    joint1.plot(ax)
+
+    ax.set_aspect("equal")
+    ax.grid()
+
+    joint3 = Joint2D(JointType.revolute, x=0, y=1, theta=90, l=1, name="joint3")
+    joint4 = Joint2D(JointType.revolute, x=0, y=2, theta=45, l=1, name="joint4")
+    robot = Robot2D([joint3, joint4])
+    robot._plot(ax)
+    plt.show()
+
+
+def test_node():
+    kc = KinematicChain()
+    kc.add_node("node1", "BASE")
+    kc.add_node("node2", "node1")
+    kc.add_node("node3", "node2")
+    kc.add_node("node4", "node2")
+    kc.add_node("node5", "node4")
+    kc.add_node("node6", "node4")
+    kc.add_node("node7", "node6")
+    s3 = kc.get_node_child_to_end("node3")
+    s4 = kc.get_node_child_to_end("node4")
+    s2 = kc.get_node_child_to_end("node2")
+    s1 = kc.get_node_child_to_end("node1")
+    print("s1", s1, "s2", s2, "s3", s3, "s4", s4)
+
+
+def test_robot():
+    joint1 = Joint2D(JointType.revolute, x=0, y=0, theta=90, l=1, name="joint1")
+    joint2 = Joint2D(JointType.revolute, x=0, y=1, theta=0, l=2, name="joint2")
+    joint3 = Joint2D(JointType.revolute, x=1, y=0, theta=90, l=1, name="joint3")
+
+    robot = Robot2D()
+    robot.add_joint(joint1, robot.base)
+    robot.add_joint(joint2, joint1)
+    robot.add_joint(joint3, robot.base)
+    robot.add_parallel_joint(
+        "joint3", "joint2", [joint3.xn, joint3.yn], JointType.revolute
+    )
+
+    robot.plot()
+    print(robot.struct)
+    # robot.forward()
+
+def test_robot_kinematics():
+    joint1 = Joint2D(JointType.revolute, x=0, y=0, theta=90, l=1, name="joint1")
+    joint2 = Joint2D(JointType.revolute, x=0, y=1, theta=0, l=2, name="joint2")
+    joint3 = Joint2D(JointType.revolute, x=1, y=0, theta=90, l=1, name="joint3")
+
+    robot = Robot2D()
+    robot.add_joint(joint1, robot.base)
+    robot.add_joint(joint2, joint1)
+    robot.add_joint(joint3, robot.base)
+    robot.plot()
+    print(robot.struct)
+    robot.forward()
+
+if __name__ == "__main__":
+    # test_robot_package()
+    # test_node()
+    test_robot()
+    # test_robot_kinematics()
+
+    print("done")
