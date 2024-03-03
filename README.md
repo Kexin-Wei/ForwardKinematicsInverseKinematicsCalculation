ff1156ce5541ecadd59e84bf068e48e28120e562
KK <wkx611@outlook.com>
2024-03-03 23:41:02 +0800

    [mod] README todos

--- README.md
+++ README.md
@@ -2,17 +2,23 @@ Robotics Demo
 ===
 # 1. Kinematics - Forward and Inverse Kinematics
 ## Fn 1.1: Symbolic Calculation using DH Table
-using symbolic math package in Python  - SymPy - to calculate forward and inverse kinematics with defined dh tables, see `1.1Kinematics_SymPy_DH.ipynb`
-- matlab code is also available in the repository, but requires symbolic math toolbox
+using symbolic math package in Python  - SymPy - to calculate forward 
+and inverse kinematics with defined dh tables, 
+see `1.1Kinematics_SymPy_DH.ipynb`
+- matlab code is also available in the repository, 
+but requires symbolic math toolbox
 
 ## Fn 1.2: Dual Quaternion Representation
--  self-defined dual quaternion class to transfer dh table to dual quaternion and calculate forward and inverse kinematics, see `1.2Kinematics_Robot.ipynb`
+-  self-defined dual quaternion class to transfer dh table 
+to dual quaternion and calculate forward and inverse kinematics, 
+see `1.2Kinematics_Robot.ipynb`
 
 
 ### Eg 1.1: 2D Serial Robot
 using example from [this website](https://motion.cs.illinois.edu/RoboticSystems/InverseKinematics.html).
 - 2D serial robot with 2 links and 2 joints
 - workspace visualization (serial #TODO)
+    - pass test_robot.py all tests #TODO
 
 ### Eg 1.2: 3D Serial Robot
 
@@ -21,18 +27,19 @@ using example from [this website](https://motion.cs.illinois.edu/RoboticSystems/
 - forward kinematics calculation (serial #TODO)
 - 3D serial robot with 3 links and 3 joints
 
-### Eg 1.3: 2D Parrallel Robot
-- 2D Parrallel Robot
-- workspace visualization ( parrallel #TODO)
+### Eg 1.3: 2D Parallel Robot
+- 2D Parallel Robot
+- workspace visualization ( parallel #TODO)
 - inverse kinematics calculation 
-- forward kinematics calculation ( parrallel#TODO)
+- forward kinematics calculation ( parallel#TODO)
 
-### Eg 1.4: 3D Parrallel Robot
+### Eg 1.4: 3D Parallel Robot
 
-# 2. Visulization
+# 2. Visualization
 ## Fn 2.1: URDF Visualization
 using [yourdfpy](https://github.com/clemense/yourdfpy/tree/main) to load robot defined in [URDF format](http://wiki.ros.org/urdf/XML).
-- "data/urdf_files": contains urdf files for different robots. [Source](https://github.com/ankurhanda/robot-assets)
+- "data/urdf_files": contains urdf files for different robots. 
+[Source](https://github.com/ankurhanda/robot-assets)
 based on yourdfpy, draw robot in 2D/3D space with matplotlib
 
 ## Fn 2.2: STL/OBJ Visualization
@@ -41,7 +48,8 @@ see implementations in `1.2Kinematics_Robot.ipynb`
 
 # 3. Control Method
 ## Eg 3.1: Kalman Filter in Motion Tracking
-with traditional Kalman Filter and Unscented Kalman Filter (filterpy package)
+with traditional Kalman Filter and Unscented Kalman Filter 
+(filterpy package)
 ![Kalman Filter](imgs/KF_motion_estimation.gif)
 
 ## Eg 3.2: Unscented Kalman Filter with 1D Pendulum
@@ -51,7 +59,7 @@ with traditional Kalman Filter and Unscented Kalman Filter (filterpy package)
 ![UKF](imgs/UKF_motion_estimation.gif)
 
 ## Eg 3.3: Particle Filter / Monte Carlo Localization in Motion Tracking
-- Pariticle initialization
+- Particle initialization
 ![Particle Initialization](imgs/particle_init.png){width=100 height=100}
 - Particle Filter estimation result (parameters sensitive)
 ![Particle Filter](imgs/PF_estimation.gif)
--- test/test_robot.py
+++ test/test_robot.py
@@ -9,16 +9,25 @@ from lib.robot.kinematic_chain import KinematicChain
 from lib.robot.joint import Joint2D, JointType
 
 
+def test_2d_serial_robot():
+    joint1 = Joint2D(
+        j_type=JointType.REVOLUTE, x=0, y=0, theta=0, l=2, name="joint1"
+    )
+    joint2 = joint1.append_joint(JointType.REVOLUTE, 0, 1, "joint2")
+    robot = Robot2D.from_joint_list([joint1, joint2])
+    robot.plot()
+
+
 def test_robot_package():
     fig, ax = plt.subplots()
-    joint1 = Joint2D(JointType.revolute, x=1, y=1, theta=90, l=1, name="joint1")
+    joint1 = Joint2D(JointType.REVOLUTE, x=1, y=1, theta=90, l=1, name="joint1")
     joint1.plot(ax)
 
     ax.set_aspect("equal")
     ax.grid()
 
-    joint3 = Joint2D(JointType.revolute, x=0, y=1, theta=90, l=1, name="joint3")
-    joint4 = Joint2D(JointType.revolute, x=0, y=2, theta=45, l=1, name="joint4")
+    joint3 = Joint2D(JointType.REVOLUTE, x=0, y=1, theta=90, l=1, name="joint3")
+    joint4 = Joint2D(JointType.REVOLUTE, x=0, y=2, theta=45, l=1, name="joint4")
     robot = Robot2D()
     robot.add_joint(joint3, robot.base)
     robot.add_joint(joint4, joint3)
@@ -27,13 +36,13 @@ def test_robot_package():
 
 def test_node():
     kc = KinematicChain()
-    kc.add_node("node1", "BASE")
-    kc.add_node("node2", "node1")
-    kc.add_node("node3", "node2")
-    kc.add_node("node4", "node2")
-    kc.add_node("node5", "node4")
-    kc.add_node("node6", "node4")
-    kc.add_node("node7", "node6")
+    kc.add_node_to_parent("node1", "BASE")
+    kc.add_node_to_parent("node2", "node1")
+    kc.add_node_to_parent("node3", "node2")
+    kc.add_node_to_parent("node4", "node2")
+    kc.add_node_to_parent("node5", "node4")
+    kc.add_node_to_parent("node6", "node4")
+    kc.add_node_to_parent("node7", "node6")
     s3 = kc.get_node_child_to_end("node3")
     s4 = kc.get_node_child_to_end("node4")
     s2 = kc.get_node_child_to_end("node2")
@@ -42,9 +51,9 @@ def test_node():
 
 
 def get_2d_serial_robot():
-    joint1 = Joint2D(JointType.revolute, x=0, y=0, theta=90, l=1, name="joint1")
-    joint2 = Joint2D(JointType.revolute, x=0, y=1, theta=0, l=2, name="joint2")
-    joint3 = Joint2D(JointType.revolute, x=1, y=0, theta=90, l=1, name="joint3")
+    joint1 = Joint2D(JointType.REVOLUTE, x=0, y=0, theta=90, l=1, name="joint1")
+    joint2 = Joint2D(JointType.REVOLUTE, x=0, y=1, theta=0, l=2, name="joint2")
+    joint3 = Joint2D(JointType.REVOLUTE, x=1, y=0, theta=90, l=1, name="joint3")
 
     robot = Robot2D()
     robot.add_joint(joint1, robot.base)
@@ -54,16 +63,16 @@ def get_2d_serial_robot():
 
 
 def get_2d_parallel_robot():
-    joint1 = Joint2D(JointType.revolute, x=0, y=0, theta=90, l=1, name="joint1")
-    joint2 = Joint2D(JointType.revolute, x=0, y=1, theta=0, l=2, name="joint2")
-    joint3 = Joint2D(JointType.revolute, x=1, y=0, theta=90, l=1, name="joint3")
+    joint1 = Joint2D(JointType.REVOLUTE, x=0, y=0, theta=90, l=1, name="joint1")
+    joint2 = Joint2D(JointType.REVOLUTE, x=0, y=1, theta=0, l=2, name="joint2")
+    joint3 = Joint2D(JointType.REVOLUTE, x=1, y=0, theta=90, l=1, name="joint3")
 
     robot = Robot2D()
     robot.add_joint(joint1, robot.base)
     robot.add_joint(joint2, joint1)
     robot.add_joint(joint3, robot.base)
     robot.add_parallel_joint(
-        "joint3", "joint2", [joint3.xn, joint3.yn], JointType.revolute
+        "joint3", "joint2", [joint3.xn, joint3.yn], JointType.REVOLUTE
     )
     return robot
 
@@ -86,16 +95,17 @@ def test_workspace():
     robot = get_2d_serial_robot()
     print(robot.struct)
     print(robot.joint_range)
-    print(robot.joint_range_per_chain)
+    print(robot.get_joint_range_per_chain)
     robot.plot_workspace()
 
 
 if __name__ == "__main__":
-    test_robot_package()
+    test_2d_serial_robot()
+    # test_robot_package()
     # test_node()
     # test_robot()
     # TODO finish workspace code by using dual quaternion
-    test_workspace()
+    # test_workspace()
     # test_robot_kinematics()
 
     print("done")
